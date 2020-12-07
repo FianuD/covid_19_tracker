a99713fc878947a5766e7df8e9b3d210bfe97f81
Fianu Dovlo <dovlofianu@gmail.com>
2020-12-07 22:04:45 +0000

    Further destructuring of data call from api

--- src/api/index.js
+++ src/api/index.js
@@ -7,13 +7,13 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
   try {
-    const { data } = await axios.get(url);
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
 
     const modifiedData = {
-      confirmed: data.confirmed,
-      recovered: data.recovered,
-      deaths: data.deaths,
-      lastUpdate: data.lastUpdate,
+      confirmed: confirmed,
+      recovered: recovered,
+      deaths: deaths,
+      lastUpdate: lastUpdate,
     }
 
     return modifiedData;
