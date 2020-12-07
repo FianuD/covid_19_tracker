bf29d01dfe8c8b9768bcd1285486cd74da68e262
Fianu Dovlo <dovlofianu@gmail.com>
2020-12-07 22:56:28 +0000

    Change data format on card to human readable format

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -22,7 +22,7 @@ const Cards = ( {data : { confirmed, recovered, deaths, lastUpdate }}) => {
                 separator=","
               />
             </Typography>
-            <Typography color="textSecondary">{lastUpdate}</Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant="body2">Number of active cases of COVID-19</Typography>
           </CardContent>
         </Grid>
