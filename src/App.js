46e50d60f451dc080a3e51470760e96bd2303697
Fianu Dovlo <dovlofianu@gmail.com>
2020-12-07 22:15:04 +0000

    Passed data to Cards component at a prop

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
 
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
