89aa5c8d82cb1cd0115bee4d9ec066ca1888e5c7
Fianu Dovlo <dovlofianu@gmail.com>
2020-12-07 21:50:42 +0000

    Export fetchdata and call in app.js

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,11 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-const fetchData = async () => {
+export const fetchData = async () => {
   try {
+    const response = await axios.get(url);
 
+    return response;
   } catch (error) {
 
   }
