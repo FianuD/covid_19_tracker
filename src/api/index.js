dd1e6aadde053c92adc4d208c992cfc6d794eba4
Fianu Dovlo <dovlofianu@gmail.com>
2020-12-07 21:46:49 +0000

    Create file for javascript functions to fetch data

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+// Functions for fetching needed data from api
+
+// axios used to make api requests
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+const fetchData = async () => {
+  try {
+
+  } catch (error) {
+
+  }
+}
