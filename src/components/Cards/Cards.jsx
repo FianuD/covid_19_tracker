788e08a69486926f24100c8bba8c6e41b98b0fb8
Fianu Dovlo <dovlofianu@gmail.com>
2020-12-07 22:48:59 +0000

    Bugfix: Only passed down data from App.js. So first destructure data and then destructure other values.

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,14 +3,17 @@ import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-u
 
 import styles from './Cards.module.css';
 
-const Cards = ({ confirmed, recovered, deaths, lastUpdate }) => {
+const Cards = ( {data : { confirmed, recovered, deaths, lastUpdate }}) => {
+  if(!confirmed) {
+    return 'Loading...';
+  }
   return(
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
         <Grid item component={Card}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Infected</Typography>
-            <Typography variant="h5">REAL DATA</Typography>
+            <Typography variant="h5">{confirmed.value}</Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant="body2">Number of active cases of COVID-19</Typography>
           </CardContent>
