e5d40818016d691fd1b526549cead9e3c1e66c95
Fianu Dovlo <dovlofianu@gmail.com>
2020-12-09 23:08:35 +0000

    Import Line chart api from react. Import style sheet.

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
 
 const Chart = () => {
   const [dailyData, setDailyData] = useState({});
@@ -11,8 +14,10 @@ const Chart = () => {
 
     fetchAPI();
   });
+  const lineChart = (
 
-  
+  );
+
   return(
     <h1>Chart</h1>
   )
