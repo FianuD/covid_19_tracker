723872a15a1c2143d4861be0888cd5f5e1e856dc
Fianu Dovlo <dovlofianu@gmail.com>
2020-12-15 21:53:31 +0000

    Styled the country picker and formatted code with prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import { Cards, Chart, CountryPicker} from './components';
-import styles from './App.module.css';
-import { fetchData } from './api';
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 class App extends React.Component {
   state = {
     data: {},
-    country: '',
-  }
+    country: "",
+  };
 
   async componentDidMount() {
     const fetchedData = await fetchData();
@@ -22,7 +22,7 @@ class App extends React.Component {
 
     //set state
     this.setState({ data: fetchedData, country: country });
-  }
+  };
 
   render() {
     const { data, country } = this.state;
@@ -33,7 +33,7 @@ class App extends React.Component {
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
       </div>
-    )
+    );
   }
 }
 
